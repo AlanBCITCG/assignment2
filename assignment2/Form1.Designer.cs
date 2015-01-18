2864f2184acbd630b6dbb66055c61990164dbc01
alan-cy-lai <alan@ergostratus.com>
2015-01-18 06:13:44 -0800

    Assignment 2 Complete

---
+++ assignment2/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace assignment2
+{
+    partial class asgn2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // asgn2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 505);
+            this.Name = "asgn2";
+            this.Text = "Lab Assignment 2";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.asgn2_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ assignment2/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace assignment2
+{
+    public partial class asgn2 : Form
+    {
+        private int s;
+
+        public asgn2()
+        {
+            InitializeComponent();
+            Text = "C4560: Assignment 2, Exercise 1 (Alan Lai Set R/2015)";
+            BackColor = Color.Black;
+            ResizeRedraw = true;
+
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        private void setS()
+        {
+            s = Math.Min(this.Size.Height, this.Size.Width);
+        }
+
+        private void asgn2_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics grfx = e.Graphics;
+            grfx.SmoothingMode = SmoothingMode.HighQuality;
+            grfx.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+            // Get value of s
+            int s = Math.Min(ClientSize.Width, ClientSize.Height);
+            // Set mid point of width and height
+            int mx = ClientSize.Width / 2;
+            int my = ClientSize.Height / 2;
+
+            double r_small = s / 12.0;
+            double r_large = s / 6.0;
+
+            // Top left section, big circle
+            double LT1x = mx - 5.0 / 12.0 * s;
+            double LT1y = my - 5.0 / 12.0 * s;
+            SolidBrush brush = new SolidBrush(Color.Blue);
+            grfx.FillEllipse(brush, (int)LT1x, (int)LT1y, (int)r_large * 2, (int)r_large * 2);
+            // small circle
+            brush = new SolidBrush(Color.Green);
+            double LT5x = mx - 1.0 / 3.0 * s;
+            double LT5y = my - 1.0 / 3.0 * s;
+            grfx.FillEllipse(brush, (int)LT5x, (int)LT5y, (int)r_small * 2, (int)r_small * 2);
+
+            // Top right section, big circle
+            brush = new SolidBrush(Color.Yellow);
+            double LT3x = mx + 1.0 / 12.0 * s;
+            double LT3y = my - 5.0 / 12.0 * s;
+            grfx.FillEllipse(brush, (int)LT3x, (int)LT3y, (int)r_large * 2, (int) r_large * 2);
+            // small circle
+            brush = new SolidBrush(Color.Green);
+            double LT7x = mx + s / 6.0;
+            double LT7y = my - 1.0 / 3.0 * s;
+            grfx.FillEllipse(brush, (int)LT7x, (int)LT7y, (int)r_small * 2, (int)r_small * 2);
+
+            // Bottom left section, big circle
+            brush = new SolidBrush(Color.Blue);
+            double LT2x = mx - 5.0 / 12.0 * s;
+            double LT2y = my + 1.0 / 12.0 * s;
+            grfx.FillEllipse(brush, (int)LT2x, (int)LT2y, (int)r_large * 2, (int)r_large * 2);
+            // small circle
+            brush = new SolidBrush(Color.Red);
+            double LT6x = mx - 1.0 / 3.0 * s;
+            double LT6y = my + s / 6.0;
+            grfx.FillEllipse(brush, (int)LT6x, (int)LT6y, (int)r_small * 2, (int)r_small * 2);
+
+
+            // Bottom right section, big circle
+            brush = new SolidBrush(Color.Yellow);
+            double LT4x = mx + 1.0 / 12.0 * s;
+            double LT4y = my + 1.0 / 12.0 * s;
+            grfx.FillEllipse(brush, (int)LT4x, (int)LT4y, (int)r_large * 2, (int)r_large * 2);
+            // small circle
+            brush = new SolidBrush(Color.Red);
+            double LT8x = mx + s / 6.0;
+            double LT8y = my + s / 6.0;
+            grfx.FillEllipse(brush, (int)LT8x, (int)LT8y, (int)r_small * 2, (int)r_small * 2);
+        }
+    }
+}
